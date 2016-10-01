c98aac6fabec92ed516a6b458b26e41a12667cdd
Dennis Hoer <dennis.hoer@pearson.com>
2016-10-01 14:05:58 -0700

    Remove :chef_gem_compile_time flag from chefspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-Chef::Config[:chef_gem_compile_time] = false
 Chef::Config[:ssl_verify_mode] = :verify_none
 
 CACHE = Chef::Config[:file_cache_path]
