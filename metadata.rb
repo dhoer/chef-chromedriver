06a134ae07f7f3d5ba97b189e3d29f39684eb859
Dennis Hoer <dennis.hoer@pearson.com>
2016-10-01 18:06:16 -0700

    Add chef_version

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,8 @@ source_url 'https://github.com/dhoer/chef-chromedriver'
 issues_url 'https://github.com/dhoer/chef-chromedriver/issues'
 version '2.0.0'
 
+chef_version '>= 12.6'
+
 supports 'centos'
 supports 'debian'
 supports 'fedora'
